fb8d191512f8d8bb4135607456ee0b16fc971425
Juan <juangaray93@gmail.com>
2019-10-26 17:05:35 -0300

    Added comments and named inline functions in helpers.js

--- helpers.js
+++ helpers.js
@@ -4,14 +4,26 @@ const normalizeText = input =>
     .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase();
 
+// Replaces the value of `template` with that of `insert`.
+const replace = (accum, { template, insert }) => accum.replace(template, insert);
+
+/* Replaces the values of `template` values with certain params.
+ * For example, `@username` is replaced by the username passed.
+ */
 const replaceTemplate = (text, { username = "" }) =>
   [
     { template: "@username", insert: username },
     { template: "@project", insert: "" } // TODO: Add projects structure
   ].reduce(
-    (accum, { template, insert }) => accum.replace(template, insert),
+    replace,
     text
   );
+
+/** Picks a random response from the provided list.
+ * Returns its message.
+ * @param { Array } r - The list of responses.
+ */
+const chooseFromResponses = r => r[Math.floor(Math.random() * r.length)].message;
 
 const pickResponse = (messages, input, username) => {
   const normalizedInput = normalizeText(input);
@@ -19,8 +31,9 @@ const pickResponse = (messages, input, username) => {
     m => m.tags.filter(t => normalizedInput.includes(t)).length
   );
   if (!responses.length) return null;
+  const response = chooseFromResponses(responses);
   return replaceTemplate(
-    responses[Math.floor(Math.random() * responses.length)].message,
+    response,
     { username }
   );
 };
